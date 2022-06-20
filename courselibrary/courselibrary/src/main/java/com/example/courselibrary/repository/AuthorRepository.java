680b15f838c3f6d7f0149a8e2f621653a8e86fc0
Ashitha2602 <80142156+Ashitha2602@users.noreply.github.com>
2022-06-20 09:12:34 +0530

    Add files via upload

---
+++ courselibrary/courselibrary/src/main/java/com/example/courselibrary/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.example.courselibrary.repository;
+
+import com.example.courselibrary.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
---
+++ courselibrary/courselibrary/src/main/java/com/example/courselibrary/service/PublisherService.java
@@ -0,0 +1,37 @@
+package com.example.courselibrary.service;
+
+import com.example.courselibrary.entity.Publisher;
+import com.example.courselibrary.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PublisherService {
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+    public List<Publisher> findAllPublishers(){
+        return publisherRepository.findAll();
+    }
+
+    public Publisher findPublisherById(Long id){
+        Publisher publisher = publisherRepository.findById(id).orElseThrow(() -> new RuntimeException("Publisher not found"));
+        return publisher;
+    }
+
+    public void cretePublisher(Publisher publisher){
+        publisherRepository.save(publisher);
+    }
+
+    public void updatePublisher(Publisher publisher){
+        publisherRepository.save(publisher);
+    }
+
+    public void deletePublisher(Long id){
+        Publisher publisher = publisherRepository.findById(id).orElseThrow(() -> new RuntimeException("Publisher not found"));
+        publisherRepository.deleteById(publisher.getId());
+    }
+}
